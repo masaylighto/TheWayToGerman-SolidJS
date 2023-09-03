78f64ee07393ed8f0e4c21b86c1d9eb76ade2639
Mohammed-Al-Mahdi <mohammed-al-mahdi@jaifari.com>
2023-09-03 13:55:45 +0300

    add the ability to send a cover

--- src/Api/DTO/CreateArticleDTO.ts
+++ src/Api/DTO/CreateArticleDTO.ts
@@ -3,11 +3,13 @@ export default class CreateArticleDTO
     Overview: string;
     Content:string;   
     CategoryID:string;
-    constructor(Title:string, Overview:string,Content:string, CategoryID:string) 
+    Picture?:string;
+    constructor(Title:string, Overview:string,Content:string, CategoryID:string,Picture:string) 
     {
         this.Title=Title;
         this.Overview=Overview;
         this.Content=Content;
         this.CategoryID=CategoryID;
+        this.Picture=Picture;
     }    
 }
--- src/routes/dashboard/articles/Article.tsx
+++ src/routes/dashboard/articles/Article.tsx
@@ -9,13 +9,26 @@ async function GetArticles()
 { 
 return []
 }
+async function ConvertFileToURL(file:File) :Promise<string>
+{
+    let result_base64 = await new Promise((resolve) => {
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => resolve(fileReader.result);
+        fileReader.readAsDataURL(file);
+    }); 
+    return result_base64 as string;
+}
 async function AddArticle(html:string)
 { 
+    let pictureField = document.getElementById("PictureField") as HTMLInputElement   
+    if (pictureField.files?.length==0){
+        return;
+    }
     let titleField = document.getElementById("TitleField") as HTMLInputElement
-    let categoryField = document.getElementById("CategoryField") as HTMLSelectElement
-    let pictureField = document.getElementById("PictureField") as HTMLInputElement
+    let categoryField = document.getElementById("CategoryField") as HTMLSelectElement    
+    let pictureAsURL= await ConvertFileToURL(pictureField.files?.item(0) as File)  
     let overviewField = document.getElementById("OverviewField") as HTMLInputElement
-    let dto = new CreateArticleDTO(overviewField.value,overviewField.value,html,categoryField.value) 
+    let dto = new CreateArticleDTO(titleField.value,overviewField.value,html,categoryField.value,pictureAsURL) 
     let response= await ArticlesApi.Add(dto)
     if ("detail" in response)
     {         
